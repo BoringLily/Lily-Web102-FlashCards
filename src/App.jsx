5d6ff91da2f3c1b84a1d67dceff2a3dc691978ef
Levko Nikitin <53998902+BoringLily@users.noreply.github.com>
2023-03-07 11:01:51 -0500

    Card Flip Animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import './App.css'
 import flashCardList from './flashCardList.js';
 const flashCards = JSON.parse(flashCardList);
 
-
-// IDK, but this keeps shuffling the values inside the objects of the array.
 function randomize(array) {
   let i = array.length;
   while (i--) {
@@ -16,40 +14,43 @@ function randomize(array) {
   return array;
 }
 
+const cardStack = randomize(flashCards[0].stackCard);
+
 function App() {
   const [cardNumber, setCardNumber] = useState(0);
+  // const [cardStack, setCardStack] = useState();
+  const [currentCard, setCurrentCard] = useState(cardStack[0]);
+  // const [nextCardRandom, setNextCardRandom] = useState(true);
   const [cardAnswer, setCardAnswer] = useState(false);
 
-  // bug in randomizer function
-  //let cardStack = randomize(flashCards[0].stackCard);
-  
-  let cardStack = flashCards[0].stackCard;
+  // let cardStack = flashCards[0].stackCard;
 
   const maxCards = cardStack.length;
 
-  const handleShowAnswer = ()=> {setCardAnswer(!cardAnswer);}
+  console.log(cardStack, cardNumber+1)
+
+  const handleShowAnswer = () => { setCardAnswer(!cardAnswer); }
 
   const handleNextCard = () => {
-    if(cardNumber+1 < maxCards)
-    {
-     setCardNumber(cardNumber+1);
-     setCardAnswer(false);
+    if (cardNumber + 1 < maxCards) {
+      setCardNumber(cardNumber + 1);
+      setCardAnswer(false);
+
+    
     }
-    else
-    {
-     console.log("This is the end.")
-      
+    else {
+      console.log("This is the end.")
     }
   };
 
   const handlePrevCard = () => {
-    if(cardNumber > 0)
-    {
-      setCardNumber(cardNumber-1);
+    if (cardNumber > 0) {
+
+      setCardNumber(cardNumber - 1);
       setCardAnswer(false);
+
     }
-    else
-    {
+    else {
       console.log("This is the beginning.");
     }
   };
@@ -59,15 +60,15 @@ function App() {
 
       <div className='card_set_title'>{flashCards[0].stackTitle}</div>
       <div className='card_set_description'>{flashCards[0].stackDescription}</div>
-      <button className="card" onClick={handleShowAnswer} >
-        <div className='card_side'>{cardAnswer?"Answer":"Question"}</div>
-        <div className='card_content'>{cardAnswer?cardStack[cardNumber].answer:cardStack[cardNumber].question}</div>
+      <button className={"card "+ (cardAnswer ? "card_back":"card_front")} onClick={handleShowAnswer} >
+        <div className={'card_side ' + (cardAnswer?"card_text_unflip":"")}>{cardAnswer ? "Answer" : "Question"}</div>
+        <div className={'card_content ' + (cardAnswer?"card_text_unflip":"")}>{cardAnswer ? cardStack[cardNumber].answer : cardStack[cardNumber].question}</div>
       </button>
 
       <div id="card_nav">
-        <button onClick={handlePrevCard} className={"card_nav_button " + (cardNumber > 0?"":"button_inactive")}> {"<--"} </button>
-        <div id='card_count'>{cardNumber+1}/{maxCards}</div>
-        <button onClick={handleNextCard} className={"card_nav_button " + (cardNumber+1 < maxCards?"":"button_inactive")}>{"-->"} </button>
+        <button onClick={handlePrevCard} className={"card_nav_button " + (cardNumber > 0 ? "" : "button_inactive")}> {"<--"} </button>
+        <div id='card_count'>{cardNumber + 1}/{maxCards}</div>
+        <button onClick={handleNextCard} className={"card_nav_button " + (cardNumber + 1 < maxCards ? "" : "button_inactive")}>{"-->"} </button>
       </div>
 
     </div>
