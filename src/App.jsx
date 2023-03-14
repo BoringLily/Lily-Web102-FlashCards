ca247f69df87659075cb6f1699b6550f145d453b
Levko Nikitin <53998902+BoringLily@users.noreply.github.com>
2023-03-13 21:25:32 -0400

    finished part 2

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,31 @@ function randomize(array) {
   return array;
 }
 
-const cardStack = randomize(flashCards[0].stackCard);
-
 function App() {
   const [cardNumber, setCardNumber] = useState(0);
-  // const [cardStack, setCardStack] = useState();
-  const [currentCard, setCurrentCard] = useState(cardStack[0]);
-  // const [nextCardRandom, setNextCardRandom] = useState(true);
+  const [cardStack, setCardStack] = useState(flashCards[0].stackCard);
   const [cardAnswer, setCardAnswer] = useState(false);
-
-  // let cardStack = flashCards[0].stackCard;
+  const [userInput, setUserInput] = useState("");
+  const [userAnswer, setUserAnswer] = useState(-1)
 
   const maxCards = cardStack.length;
 
-  console.log(cardStack, cardNumber+1)
-
   const handleShowAnswer = () => { setCardAnswer(!cardAnswer); }
+
+  const handleShuffle = () => {
+    setCardStack(randomize(flashCards[0].stackCard));
+    setCardNumber(0);
+    setCardAnswer(false);
+    setUserInput(""); 
+    setUserAnswer(-1);
+  }
 
   const handleNextCard = () => {
     if (cardNumber + 1 < maxCards) {
       setCardNumber(cardNumber + 1);
       setCardAnswer(false);
-
-    
+      setUserInput(""); 
+      setUserAnswer(-1);
     }
     else {
       console.log("This is the end.")
@@ -45,33 +47,64 @@ function App() {
 
   const handlePrevCard = () => {
     if (cardNumber > 0) {
-
       setCardNumber(cardNumber - 1);
       setCardAnswer(false);
-
+      setUserInput(""); 
+      setUserAnswer(-1);
     }
     else {
       console.log("This is the beginning.");
     }
   };
   
+  const handleCheckAnswer = () => {
+    let answer = cardStack[cardNumber].answer.toLowerCase().split(" ");
+    let userAnswer = userInput.toLowerCase();
+    let match = 0;
+    
+    answer.forEach((element)=>{ 
+    if(userAnswer.includes(element))
+    {
+      match += 1;
+    }
+    })
+    
+      console.log(match, answer.length, match/answer.length * 100);
+      setUserAnswer(match/answer.length*100);
+  };
+
   return (
     <div className="App">
 
       <div className='card_set_title'>{flashCards[0].stackTitle}</div>
       <div className='card_set_description'>{flashCards[0].stackDescription}</div>
-      <button className="card" onClick={handleShowAnswer} >
-        <div className={"flip_card "+ (cardAnswer ? "card_back":"card_front")}></div>
-        <div className='card_side '>{cardAnswer ? "Answer" : "Question"}</div>
-        <div className='card_content'>{cardAnswer ? cardStack[cardNumber].answer : cardStack[cardNumber].question}</div>
+
+      <button className="card" onClick={handleShowAnswer}> 
+        <div className={'flip_card'+(cardAnswer?" showFlip":"")}>
+          <div className={'card_front' +(cardAnswer?" card_hide":"")}>
+            <div className='card_side'>Question</div>
+            <div className='card_content'>{cardStack[cardNumber].question}</div>
+          </div>
+
+          <div className={'card_back' +(!cardAnswer?" card_hide":"")}> 
+            <div className='card_side'>Answer</div>
+            <div className='card_content'>{cardStack[cardNumber].answer}</div>
+          </div>
+        </div>
       </button>
+
+      <div id="AnswerInput">
+      <div className='answer_title' >{(userAnswer>-1?`Your answer is ${userAnswer}% correct`:"Enter your answer")}</div>
+        <input className="answer_box" type="text" placeholder="Enter Answer" value={userInput} onChange={(event)=>{setUserInput(event.target.value); setUserAnswer(-1)}} />
+        <button className="check_answer_button" onClick={handleCheckAnswer}>Check</button>
+      </div>
 
       <div id="card_nav">
         <button onClick={handlePrevCard} className={"card_nav_button " + (cardNumber > 0 ? "" : "button_inactive")}> {"<--"} </button>
         <div id='card_count'>{cardNumber + 1}/{maxCards}</div>
         <button onClick={handleNextCard} className={"card_nav_button " + (cardNumber + 1 < maxCards ? "" : "button_inactive")}>{"-->"} </button>
       </div>
-
+      <button className='card_nav_button' onClick={handleShuffle}>Restart and Shuffle</button>
     </div>
   )
 }
--- src/flashCardList.js
+++ src/flashCardList.js
@@ -9,43 +9,43 @@ const flashCardList = `[
         [ 
             {
                 "question": "What is a computer?",
-                "answer": "An electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program."
+                "answer": "An electronic device for storing and processing data."
             },
             {
-                "question": "What is a CPU?",
-                "answer": "A Central Procesing Unit is a digital circuit that fetches, decodes and executes instructions."
+                "question": "What does CPU stand for?",
+                "answer": "Central Processing Unit"
             },
             {
-                "question": "Computer Architecture",
-                "answer": "In computer engineering, computer architecture is a description of the structure of a computer system made from component parts. It can sometimes be a high-level description that ignores details of the implementation."
+                "question": "What is Computer Architecture?",
+                "answer": "A description of the structure of a computer system made from component parts."
             },
             {
-                "question": "what is RISC?",
-                "answer": "A reduced instruction set computer (RISC) is a computer architecture designed to simplify the individual instructions given to the computer to accomplish tasks."
+                "question": "What does RISC stand for?",
+                "answer": "Reduced Instruction Set Computer"
             },
             {
-                "question": "What is a CISC architecture?",
-                "answer": "A computer architecture in which single instructions can execute several low-level operations (such as a load from memory, an arithmetic operation, and a memory store) or are capable of multi-step operations or addressing modes within single instructions."
+                "question": "What does CISC stand for?",
+                "answer": "Complex Instruction Set Computer"
             },
             {
-                "question": "Instruction Pipelining",
-                "answer": "Instruction pipelining is a technique for implementing instruction-level parallelism within a single processor."
+                "question": "What is Instruction Pipelining?",
+                "answer": "A technique for implementing instruction-level parallelism within a single processor."
             },
             {
-                "question": "Logic Gate",
+                "question": "What is a Logic Gate?",
                 "answer": "An idealized or physical device that performs a Boolean function, a logical operation performed on one or more binary inputs that produces a single binary output."
             },
             {
                 "question": "What does MOSFET stand for?",
-                "answer": "metal–oxide–semiconductor field-effect transistor."
+                "answer": "Metal–Oxide–Semiconductor Field-Effect Transistor."
             },
             {
                 "question": "What is Boolean Logic?",
                 "answer": "Boolean Logic is a form of algebra that is centered around three simple words known as Boolean Operators: “Or,” “And,” and “Not.” "
             },
             {
-                "question": "The von Neumann architecture",
-                "answer": "A computer architecture that consists of A processing unit (ALU and Registers), A control unit (instruction register and a program counter), Memory (data and instructions), External mass storage, and I/O mechanisms"
+                "question": "What are the elements of a von Neumann architecture",
+                "answer": "Processing unit (ALU and Registers), Control unit (instruction register and a program counter), Memory (data and instructions), External mass storage, and I/O mechanisms"
             }
         ]
     }
